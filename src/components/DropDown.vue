f84a2aed1986e5891ea694879f0cf3534d137e5e
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-27 23:46:59 -0300

    including dropdown menu

---
+++ src/components/DropDown.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="var(--yellowHibredu)"
+          dark
+          v-bind="attrs"
+          v-on="on">
+          V
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index">
+          <v-list-item-icon>
+            <v-icon color="var(--grayHibredu)">
+              {{ item.ico }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title class="title">{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { ico: 'mdi-account-circle', title: 'Meu Perfil' },
+        { ico: 'mdi-exit-to-app', title: 'Sair' },
+      ],
+    }),
+  }
+</script>
+
+<style>
+div {
+  font-family: 'Metropolis Regular';
+  color: var(--grayHibredu);
+}
+</style>
