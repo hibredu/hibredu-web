36e188bf0bf62cd2309cea41636e03543e751571
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-30 18:39:54 -0300

    including top bar

---
+++ src/components/Bars/TopBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="blue-bar-top">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TopBar',
+}
+</script>
+
+<style scoped>
+.blue-bar-top{
+  width: 100%;
+  height: 30%;
+  background-color: var(--blueHibredu);
+  position: absolute;
+  z-index: -1;
+}
+</style>
