e7826aea47576deebd29496244a1cab2ea0554cc
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-30 18:40:52 -0300

    including search bar

---
+++ src/components/Bars/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-bar">
+    <v-toolbar
+      color="var(--lightBlueHibredu)"
+      dense
+      flat
+      rounded
+    >
+      <v-btn icon color="var(--grayHibredu)">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <input type="text" placeholder="Pesquisar"/>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+}
+</script>
+
+<style>
+.search-bar input{
+  font-family: 'Metropolis Regular';
+}
+</style>
