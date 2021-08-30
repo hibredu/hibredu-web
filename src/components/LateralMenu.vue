a606738530b37c5a7c8de9b03bdef7e21c24ca91
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-30 18:41:02 -0300

    including lateral menu

---
+++ src/components/LateralMenu.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="lateral-menu">
+    <v-navigation-drawer
+      permanent
+      expand-on-hover
+    >
+      <v-list nav dense>
+        <v-list-item>
+          <!--<v-img class="logo" src="../assets/images/hibredu-logo.png"></v-img>-->
+        </v-list-item>
+
+        <v-list-item link
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <v-list-item-icon>
+            <v-icon color="var(--grayHibredu)">{{ item.ico }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title color="var(--grayHibredu)">
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-group
+          prepend-icon="mdi-google-analytics"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Análise</v-list-item-title>
+          </template>
+          
+          <v-list-group
+            no-action
+            sub-group
+            prepend-icon="mdi-account-group"
+          > 
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Turma</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-group>
+          <v-list-group
+            no-action
+            sub-group
+            prepend-icon="mdi-account "
+            color="var(--grayHibredu)"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Aluno</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-group>
+        </v-list-group>
+              <v-list-group
+          prepend-icon="mdi-cloud-download"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Exportar</v-list-item-title>
+          </template>
+          
+          <v-list-group
+            no-action
+            sub-group
+            prepend-icon="mdi-clipboard-list-outline"
+          > 
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Atividades</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-group>
+          <v-list-group
+            no-action
+            sub-group
+            prepend-icon="mdi-list-status"
+            color="var(--grayHibredu)"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Presença</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-group>
+        </v-list-group>
+        </v-list>
+      </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        { ico: 'mdi-home', title: 'Home' },
+        { ico: 'mdi-cloud-upload', title: 'Importar' },
+      ],
+    }),
+  }
+</script>
+
+<style scoped>
+.lateral-menu {
+  height: 100%;
+  font-family: 'Metropolis Regular';
+  color: var(--grayHibredu);
+}
+
+</style>
