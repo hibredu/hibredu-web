bc3b310f2f94a738a72c07998fcdb3becefc9307
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-30 18:40:27 -0300

    including principal dashboard

---
+++ src/views/Dashboard/PrincipalDashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="principal-dashboard">
+    <LateralMenu/>
+    <TopBar/>
+    <SearchBar/>
+    <DropDown/>
+  </div>
+</template>
+
+<script>
+import LateralMenu from '../../components/LateralMenu.vue'
+import DropDown from '../../components/DropDown.vue'
+import TopBar from '../../components/Bars/TopBar.vue'
+import SearchBar from '../../components/Bars/SearchBar.vue'
+
+
+export default {
+name: 'PrincipalDashboard',
+
+  components: {
+    LateralMenu, DropDown, TopBar, SearchBar
+  },
+}
+</script>
+
+<style>
+.principal-dashboard{
+  width: 100%;
+  height: 100%;
+  background-color: var(--lightBlueHibredu);
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  z-index: 1;
+  align-items: top;
+}
+
+</style>
