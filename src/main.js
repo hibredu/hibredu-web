726b34abb5c962768401c6d699c7dd12cf59caad
Giovanna Godoy <giovannagodoy@icloud.com>
2021-08-30 18:41:37 -0300

    including vue router configs

--- src/main.js
+++ src/main.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import './assets/scss/app.scss';
+import VueRouter from 'vue-router'
+import { routes } from '../src/router/routes';
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes : routes
+});
+
 
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
