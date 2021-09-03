b508fb7ce817015f360e3ed33ebd972a2a95b244
Giovanna Godoy <giovannagodoy@icloud.com>
2021-09-03 09:52:07 -0300

    including component - Logo

---
+++ src/components/logo/Logo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="logo">
+        <img src="../../assets/images/hibredu-logotipo.png">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Logo'
+}
+</script>
+
+<style>
+.logo {
+    width: 40%;
+}
+
+.logo img{
+    width: 100%;
+}
+@media only screen and (max-width: 1024px){
+    .logo {
+        width: 80%;
+        height: auto;
+    }
+
+    .logo img{
+        width: 100%;
+    }
+}
+</style>
