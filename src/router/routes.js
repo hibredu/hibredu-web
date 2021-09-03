89db5a94523b4ea329b8f19748e5acb1ba20c719
Giovanna Godoy <giovannagodoy@icloud.com>
2021-09-03 09:52:30 -0300

    upadting routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
 import Login from '../views/Login/Login';
 import PrincipalDashboard from '../views/Dashboard/PrincipalDashboard';
+import Registration from '../views/Registration/Registration';
 
 export const routes = [
 
     { path: '', component: Login },
     { path: '/home', component: PrincipalDashboard },
+    { path: '/register', component: Registration },
 
 ];
