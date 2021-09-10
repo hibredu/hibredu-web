5cb80f3227e775fe36782e6353082e06f5c51329
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-10 20:06:48 -0300

    fix: adjusting indentation

--- src/components/LateralMenu.vue
+++ src/components/LateralMenu.vue
@@ -1,21 +1,18 @@
 <template>
   <div class="lateral-menu">
-    <v-navigation-drawer
-      permanent
-      expand-on-hover
-      width="250"
-    >
+    <v-navigation-drawer permanent expand-on-hover width="250">
       <v-list nav dense>
         <v-list-item class="logo">
           <v-list-item-icon>
-            <v-img src="../assets/images/hibredu-logotipo.png" height="40" class="ml-auto"></v-img>
+            <v-img
+              src="../assets/images/hibredu-logotipo.png"
+              height="40"
+              class="ml-auto"
+            ></v-img>
           </v-list-item-icon>
         </v-list-item>
 
-        <v-list-item link
-          v-for="(item, index) in items"
-          :key="index"
-        >
+        <v-list-item link v-for="(item, index) in items" :key="index">
           <v-list-item-icon>
             <v-icon color="var(--grayHibredu)">{{ item.ico }}</v-icon>
           </v-list-item-icon>
@@ -30,12 +27,8 @@
           <template v-slot:activator>
             <v-list-item-title>Análise</v-list-item-title>
           </template>
-          
-          <v-list-group
-            no-action
-            sub-group
-            prepend-icon="mdi-account-group"
-          > 
+
+          <v-list-group no-action sub-group prepend-icon="mdi-account-group">
             <template v-slot:activator>
               <v-list-item-content>
                 <v-list-item-title>Turma</v-list-item-title>
@@ -62,13 +55,13 @@
           <template v-slot:activator>
             <v-list-item-title>Exportar</v-list-item-title>
           </template>
-          
+
           <v-list-group
             no-action
             sub-group
             prepend-icon="mdi-clipboard-list-outline"
             color="var(--grayHibredu)"
-          > 
+          >
             <template v-slot:activator>
               <v-list-item-content>
                 <v-list-item-title>Atividades</v-list-item-title>
@@ -94,20 +87,19 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      items: [
-        { ico: 'mdi-home', title: 'Home' },
-        { ico: 'mdi-cloud-upload', title: 'Importar' },
-      ],
-    }),
-  }
+export default {
+  data: () => ({
+    items: [
+      { ico: "mdi-home", title: "Home" },
+      { ico: "mdi-cloud-upload", title: "Importar" },
+    ],
+  }),
+};
 </script>
 
 <style scoped>
 div {
   color: var(--grayHibredu);
-  font-family: 'Metropolis Regular';
+  font-family: "Metropolis Regular";
 }
-
 </style>
