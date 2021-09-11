ba2a3f318f27eaf4cea158b2494b6a4295df0eea
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-11 01:24:43 -0300

    fix: including vuex config

---
+++ src/services/axios.js
@@ -0,0 +1,25 @@
+import configAxios from 'axios';
+
+const apiInstance = configAxios.create({
+    baseURL: 'http://localhost:8080/',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+});
+
+apiInstance.interceptors.request.use((config) => {
+    const bearerToken = localStorage.getItem('access_token');
+    if (
+        bearerToken
+        || bearerToken !== 'undefined'
+        || bearerToken !== ''
+        || bearerToken != null
+    ) {
+        config.headers.Authorization = `Bearer ${bearerToken}`;
+    }
+    return config;
+});
+
+export default apiInstance;
