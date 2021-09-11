3657a07ce1f342ee1b03a53973a4153022afee5a
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-11 01:25:16 -0300

    feat: including globalMethods

---
+++ src/mixins/globalMethods.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  methods: {
+    validateEmail(string) {
+      let user = string.value.substring(0, string.value.indexOf("@"));
+      let domain = string.value.substring(
+        string.value.indexOf("@") + 1,
+        string.value.length
+      );
+
+      if (
+        user.length >= 1 &&
+        domain.length >= 3 &&
+        user.search("@") == -1 &&
+        domain.search("@") == -1 &&
+        user.search(" ") == -1 &&
+        domain.search(" ") == -1 &&
+        domain.search(".") != -1 &&
+        domain.indexOf(".") >= 1 &&
+        domain.lastIndexOf(".") < domain.length - 1
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
