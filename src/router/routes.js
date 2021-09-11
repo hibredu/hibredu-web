0625bdf7fa413fc43e2140d842509235b7f0d4df
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-11 01:25:03 -0300

    feat: including new route

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,13 @@
 import Login from '../views/Login/Login';
 import PrincipalDashboard from '../views/Dashboard/PrincipalDashboard';
 import Registration from '../views/Registration/Registration';
+import Importation from '../views/Importation/Importation';
 
 export const routes = [
 
     { path: '', component: Login },
     { path: '/home', component: PrincipalDashboard },
     { path: '/register', component: Registration },
+    { path: '/import', component: Importation },
 
 ];
