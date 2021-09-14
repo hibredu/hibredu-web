c71fd4a2011470640c85c1f06dc93a7c20d556ea
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-14 20:22:45 -0300

    fix: including new routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,12 +2,12 @@ import Login from '../views/Login/Login';
 import PrincipalDashboard from '../views/Dashboard/PrincipalDashboard';
 import Registration from '../views/Registration/Registration';
 import Importation from '../views/Importation/Importation';
+import ImportActivity from '../views/Importation/ImportActivity';
 
 export const routes = [
-
-    { path: '', component: Login, name: 'login' },
+    { path: '/', component: Login, name: 'login' },
     { path: '/home', component: PrincipalDashboard, name: 'home' },
     { path: '/register', component: Registration, name: 'register' },
     { path: '/import', component: Importation, name: 'import' },
-
+    { path: '/import/list', component: ImportActivity, name: 'import_list' },
 ];
