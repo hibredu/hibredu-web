3f6e0c0c0f92da1a71b19d256c72298b0017c6eb
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-15 15:03:18 -0300

    style: updating size

--- src/components/bars/TopBar.vue
+++ src/components/bars/TopBar.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="blue-bar-top">
-  </div>
+  <div class="blue-bar-top"></div>
 </template>
 
 <script>
 export default {
-    name: 'TopBar',
-}
+  name: "TopBar",
+};
 </script>
 
 <style scoped>
-.blue-bar-top{
+.blue-bar-top {
   width: 100%;
-  height: 37%;
+  height: 400px;
   background-color: var(--blueHibredu);
   position: absolute;
   z-index: -1;
+}
+
+@media only screen and (max-width: 1024px) {
+  .blue-bar-top {
+    width: 100%;
+    height: 37%;
+    background-color: var(--blueHibredu);
+    position: absolute;
+    z-index: -1;
+  }
 }
 </style>
