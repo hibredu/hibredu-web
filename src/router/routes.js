503eeb5fcebd34a14f75ec09495a44735ce78463
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-15 15:02:44 -0300

    including new routes and components

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,7 @@
 import Login from '../views/Login/Login';
 import PrincipalDashboard from '../views/Dashboard/PrincipalDashboard';
+import StudentDashboard from '../views/Dashboard/StudentDashboard';
+import ClassroomDashboard from '../views/Dashboard/ClassroomDashboard';
 import Registration from '../views/Registration/Registration';
 import Importation from '../views/Importation/Importation';
 import ImportActivity from '../views/Importation/ImportActivity';
@@ -7,6 +9,8 @@ import ImportActivity from '../views/Importation/ImportActivity';
 export const routes = [
     { path: '/', component: Login, name: 'login' },
     { path: '/home', component: PrincipalDashboard, name: 'home' },
+    { path: '/student', component: StudentDashboard, name: 'student' },
+    { path: '/classroom', component: ClassroomDashboard, name: 'classroom' },
     { path: '/register', component: Registration, name: 'register' },
     { path: '/import', component: Importation, name: 'import' },
     { path: '/import/list', component: ImportActivity, name: 'import_list' },
---
+++ src/views/Dashboard/ClassroomDashboard.vue
@@ -0,0 +1,184 @@
+<template>
+  <div class="principal-dashboard">
+    <TopBar />
+    <LateralMenu />
+    <div class="content">
+      <div class="top-bar">
+        <SearchBar />
+        <DropDown />
+      </div>
+      <div class="welcome-bar">
+        <WelcomeBar name="Vera" />
+      </div>
+
+      <div class="cards">
+        <InfoCard
+          text="Atividades Cadastradas"
+          number="210"
+          color="color: var(--blueAlert)"
+        />
+        <InfoCard
+          text="Porcentagem de Entrega"
+          number="85%"
+          color="color: var(--greenAlert)"
+        />
+        <InfoCard
+          text="Taxa de Acerto"
+          number="90%"
+          color="color: var(--greenAlert)"
+        />
+        <InfoCard text="Alertas" number="42" color="color: var(--redAlert)" />
+      </div>
+      <div class="middle">
+        <ActivityCard2/>
+        <PerformanceCard2/>
+      </div>
+      <div class="bottom">
+        <AlertCard />
+        <PerformanceCard />
+        <ActivityCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LateralMenu from "../../components/LateralMenu";
+import DropDown from "../../components/DropDown";
+import TopBar from "../../components/bars/TopBar";
+import SearchBar from "../../components/bars/SearchBar";
+import WelcomeBar from "../../components/bars/WelcomeBar";
+import InfoCard from "../../components/cards/InfoCard";
+import AlertCard from "../../components/cards/alerts/AlertCard";
+import PerformanceCard from "../../components/cards/alerts/PerformanceCard";
+import ActivityCard from "../../components/cards/alerts/ActivityCard";
+import PerformanceCard2 from "../../components/graphs/PerformanceCard2";
+import ActivityCard2 from "../../components/graphs/ActivityCard2";
+// import { mapActions } from "vuex";
+
+export default {
+  name: "ClassroomDashboard",
+
+  components: {
+    LateralMenu,
+    DropDown,
+    TopBar,
+    SearchBar,
+    WelcomeBar,
+    InfoCard,
+    AlertCard,
+    PerformanceCard,
+    ActivityCard,
+    PerformanceCard2,
+    ActivityCard2,
+  },
+};
+</script>
+
+<style scoped>
+.principal-dashboard {
+  width: 100%;
+  height: auto;
+  background-color: var(--lightBlueHibredu);
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  z-index: 1;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 2em;
+}
+
+.top-bar {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100px;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.cards {
+  margin-top: 1em;
+  height: 10em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.welcome-bar {
+  margin-top: 10px;
+}
+
+.middle {
+  margin-top: 1em;
+  height: 25em;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.bottom {
+  margin-top: 1em;
+  height: 25em;
+  width: auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+@media only screen and (max-width: 1024px) {
+  .principal-dashboard {
+    width: 100%;
+    height: auto;
+    background-color: var(--lightBlueHibredu);
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    z-index: 1;
+  }
+
+  .top-bar {
+    width: auto;
+    margin-top: 2em;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    width: 80%;
+    justify-content: center;
+    height: 100%;
+    padding: 1em;
+  }
+
+  .cards {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+  }
+
+  .middle {
+    height: 25em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: auto;
+  }
+
+  .bottom {
+    height: 45em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: auto;
+  }
+}
+</style>
