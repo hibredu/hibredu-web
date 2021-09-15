64faab05a49b35093d47d142d484e70eca57d503
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-14 21:45:48 -0300

    feat: inclduing loading component

---
+++ src/components/loading/DefaultLoading.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="text-center">
+    <v-progress-circular
+      :size="70"
+      indeterminate
+      color="var(--yellowHibredu)"
+    ></v-progress-circular>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'DefaultLoading',
+}
+</script>
+
+<style scoped>
+.v-progress-circular {
+  margin: 1rem;
+}
+</style>
