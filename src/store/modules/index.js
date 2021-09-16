522285843ece5d097b3bab63036187b8a4c42797
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-15 21:07:12 -0300

    updating action

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,17 +2,12 @@ import { auth, createTeacher, overviewClassroom } from "../../services/index"
 
 export const indexStore = {
     state: () => ({
-        token: '',
     }),
     mutations: {
-        SET_TOKEN(state, payload) {
-            state.token = payload;
-        },
     },
     actions: {
         async action_auth(context, payload) {
             return await auth(payload).then(response => {
-                context.commit("SET_TOKEN", response.data.token);
                 return response;
             }).catch(err => console.error(err));
         },
@@ -23,7 +18,7 @@ export const indexStore = {
         },
         async action_overviewClassroom(context, payload) {
             return await overviewClassroom(payload).then(response => {
-                return response;
+                return response.data;
             }).catch(err => console.error(err));
         },
     },
