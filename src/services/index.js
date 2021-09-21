efe34becc42c310c5860f6be22e28548c5edf060
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-21 18:54:57 -0300

    updating files

--- src/services/index.js
+++ src/services/index.js
@@ -2,59 +2,86 @@ import axios from "./axios";
 
 const url = 'https://hibredu-api.herokuapp.com'
 
-export const auth = (params) => {
-    return axios.post(`${url}/auth`, params);
-};
-
+/*
+    Teacher
+*/
 export const createTeacher = (params) => {
     return axios.post(`${url}/teacher`, params);
 };
 
-export const overviewClassroom = (params) => {
-    return axios.get(`${url}/overview/classroom`, params);
+
+/*
+    Auth
+*/
+export const auth = (params) => {
+    return axios.post(`${url}/auth`, params);
 };
 
-export const classroom = (params) => {
-    return axios.get(`${url}/classroom`, params);
-};
 
-export const overviewActivities = (params) => {
-    return axios.get(`${url}/overview/activities`, params);
+/*
+    Student
+*/
+export const student = (params) => {
+    return axios.get(`${url}/student`, params);
 };
-
-export const overviewAttendance = (params) => {
-    return axios.get(`${url}/overview/attendance`, params);
-};
-
-export const overviewAlerts = (params) => {
-    return axios.get(`${url}/overview/alerts`, params);
-};
-
-export const school = (params) => {
-    return axios.get(`${url}/school`, params);
-};
-
-export const classroomBySchoolId = (params) => {
-    return axios.get(`${url}/school/${params.schoolId}/classrooms`, params);
-};
-
-export const alertByClassroomId = (params) => {
-    return axios.get(`${url}/alert/class/${params.classroomId}`, params);
-};
-
-export const overviewAttendanceActivities = (params) => {
-    return axios.get(`${url}/overview/attendance/activities`, params);
-};
-
-export const classroomById = (params) => {
-    return axios.get(`${url}/classroom/${params.classroomId}`, params);
-};
-
 export const studentById = (params) => {
     return axios.get(`${url}/student/${params.studentId}`, params);
 };
 
+
+/*
+    Classroom
+*/
+export const classroom = (params) => {
+    return axios.get(`${url}/classroom`, params);
+};
+export const classroomById = (params) => {
+    return axios.get(`${url}/classroom/${params.classroomId}`, params);
+};
+
+
+/*
+    Attendance
+*/
+
+
+/*
+    Alert
+*/
+export const alertByClassroomId = (params) => {
+    return axios.get(`${url}/alert/class/${params.classroomId}`, params);
+};
 export const alertByStudentId = (params) => {
     return axios.get(`${url}/alert/student/${params.studentId}`, params);
 };
 
+
+/*
+    School
+*/
+export const school = (params) => {
+    return axios.get(`${url}/school`, params);
+};
+export const classroomBySchoolId = (params) => {
+    return axios.get(`${url}/school/${params.schoolId}/classrooms`, params);
+};
+
+
+/*
+    Overview
+*/
+export const overviewAttendance = (params) => {
+    return axios.get(`${url}/overview/attendance`, params);
+};
+export const overviewActivities = (params) => {
+    return axios.get(`${url}/overview/activities`, params);
+};
+export const overviewClassroom = (params) => {
+    return axios.get(`${url}/overview/classroom`, params);
+};
+export const overviewAlerts = (params) => {
+    return axios.get(`${url}/overview/alerts`, params);
+};
+export const overviewAttendanceActivities = (params) => {
+    return axios.get(`${url}/overview/attendance/activities`, params);
+};
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,61 +1,120 @@
 import {
-    auth,
     createTeacher,
-    overviewClassroom,
+    auth,
+    student,
+    studentById,
     classroom,
-    overviewActivities,
-    overviewAttendance,
+    classroomById,
+    alertByClassroomId,
+    alertByStudentId,
     school,
     classroomBySchoolId,
-    alertByClassroomId,
+    overviewAttendance,
+    overviewActivities,
+    overviewClassroom,
+    overviewAlerts,
     overviewAttendanceActivities,
-    classroomById,
-    studentById,
-    alertByStudentId,
-    overviewAlerts
 } from "../../services/index"
 
 export const indexStore = {
     state: () => ({
+        token: '',
+        teacher: {
+            id: '',
+            name: ''
+        },
+        classrooms: []
     }),
     mutations: {
+        SET_TOKEN(state, payload) {
+            state.token = payload;
+        },
+        SET_TEACHER(state, payload) {
+            state.teacher.id = payload.id;
+            state.teacher.name = payload.name;
+        },
+        SET_CLASSROOMS(state, payload) {
+            state.classrooms = payload
+        }
     },
     actions: {
-        async action_auth(context, payload) {
-            return await auth(payload).then(response => {
-                return response;
-            }).catch(err => console.error(err));
-        },
+        /*
+            Teacher
+        */
         async action_createTeacher(context, payload) {
             return await createTeacher(payload).then(response => {
                 return response;
             }).catch(err => console.error(err));
         },
-        async action_overviewClassroom(context, payload) {
-            return await overviewClassroom(payload).then(response => {
-                return response.data;
-            }).catch(err => { console.error(err) });
+
+
+        /*
+            Auth
+        */
+        async action_auth(context, payload) {
+            return await auth(payload).then(response => {
+                context.commit("SET_TOKEN", response.data.token);
+                context.commit("SET_TEACHER", response.data.teacher);
+                return response;
+            }).catch(err => console.error(err));
         },
+
+
+        /*
+            Student
+        */
+        async action_student(context, payload) {
+            return await student(payload).then(response => {
+                return response.data;
+            }).catch(err => console.error(err));
+        },
+        async action_studentById(context, payload) {
+            return await studentById(payload).then(response => {
+                return response.data;
+            }).catch(err => console.error(err));
+        },
+
+
+        /*
+            Classroom
+        */
         async action_classroom(context, payload) {
+            context.commit("SET_CLASSROOMS", []);
             return await classroom(payload).then(response => {
+                context.commit("SET_CLASSROOMS", response.data);
                 return response.data;
             }).catch(err => console.error(err));
         },
-        async action_overviewActivities(context, payload) {
-            return await overviewActivities(payload).then(response => {
+        async action_classroomById(context, payload) {
+            return await classroomById(payload).then(response => {
                 return response.data;
             }).catch(err => console.error(err));
         },
-        async action_overviewAttendance(context, payload) {
-            return await overviewAttendance(payload).then(response => {
+
+
+        /*
+            Attendance
+        */
+
+
+        /*
+            Alert
+        */
+        async action_alertByClassroomId(context, payload) {
+            return await alertByClassroomId(payload).then(response => {
                 return response.data;
             }).catch(err => console.error(err));
         },
-        async action_overviewAlerts(context, payload) {
-            return await overviewAlerts(payload).then(response => {
+        async action_alertByStudentId(context, payload) {
+            return await alertByStudentId(payload).then(response => {
                 return response.data;
             }).catch(err => console.error(err));
         },
+
+
+        /*
+            School
+        */
         async action_school(context, payload) {
             return await school(payload).then(response => {
                 return response.data;
@@ -66,28 +125,33 @@ export const indexStore = {
                 return response.data;
             }).catch(err => console.error(err));
         },
-        async action_alertByClassroomId(context, payload) {
-            return await alertByClassroomId(payload).then(response => {
+
+
+        /*
+            Overview
+        */
+        async action_overviewAttendance(context, payload) {
+            return await overviewAttendance(payload).then(response => {
+                return response.data;
+            }).catch(err => console.error(err));
+        },
+        async action_overviewActivities(context, payload) {
+            return await overviewActivities(payload).then(response => {
+                return response.data;
+            }).catch(err => console.error(err));
+        },
+        async action_overviewClassroom(context, payload) {
+            return await overviewClassroom(payload).then(response => {
+                return response.data;
+            }).catch(err => { console.error(err) });
+        },
+        async action_overviewAlerts(context, payload) {
+            return await overviewAlerts(payload).then(response => {
                 return response.data;
             }).catch(err => console.error(err));
         },
         async action_overviewAttendanceActivities(context, payload) {
             return await overviewAttendanceActivities(payload).then(response => {
-                return response.data;
-            }).catch(err => console.error(err));
-        },
-        async action_classroomById(context, payload) {
-            return await classroomById(payload).then(response => {
-                return response.data;
-            }).catch(err => console.error(err));
-        },
-        async action_studentById(context, payload) {
-            return await studentById(payload).then(response => {
-                return response.data;
-            }).catch(err => console.error(err));
-        },
-        async action_alertByStudentId(context, payload) {
-            return await alertByStudentId(payload).then(response => {
                 return response.data;
             }).catch(err => console.error(err));
         },
