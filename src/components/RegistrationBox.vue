77ee2ad30dbf5ee45e8f200867b62f2635cc6937
Giovanna Godoy <giovanna.godoy@jazztech.com.br>
2021-09-27 23:12:24 -0300

    updating register component

--- src/components/RegistrationBox.vue
+++ src/components/RegistrationBox.vue
@@ -2,49 +2,64 @@
   <v-card class="registration" flat>
     <v-form ref="form" lazy-validation>
       <EmailInput
-        @update:value="email = $event"
+        @update:value="email = $event;validateFields()"
         ico="mdi-email"
         label="E-mail"
         type="text"
       />
       <TextInput
-        @update:value="name = $event"
+        @update:value="name = $event;validateFields()"
         ico="mdi-account-circle"
         label="Nome Completo"
         type="text"
       />
       <TextInput
-        @update:value="phone = $event"
+        @update:value="phone = $event;validateFields()"
         ico="mdi-phone"
         label="Telefone"
         type="text"
       />
       <SelectInput
-        @update:value="selectedEducationalInstitution = $event;getClassroomsBySchoolId()"
+        @update:value="
+          selectedEducationalInstitution = $event;
+          getClassroomsBySchoolId();
+          validateFields()
+        "
         :items="this.educationalInstitution"
         ico="mdi-school"
         label="Instituição de Ensino"
         type="text"
       />
+      <SelectCheckBoxInput
+        @update:value="formatClassrooms2Register($event);validateFields()"
+        v-if="
+          this.selectedEducationalInstitution != null && this.classrooms != null
+        "
+        :items="this.classrooms"
+        ico="mdi-account-group"
+        label="Turmas"
+        type="text"
+      />
       <div class="password">
         <PasswordInput
-          @update:value="password = $event"
+          @update:value="password = $event;validateFields()"
           ico="mdi-key-variant"
           label="Senha"
           type="'password'"
         />
         <PasswordInput
-          @update:value="password2 = $event"
+          @update:value="password2 = $event;validateFields()"
           ico="mdi-key-variant"
           label="Confirme a senha"
           type="'password'"
         />
       </div>
       <CheckBox
-        @click.native="agree = !agree"
+        @click.native="agree = !agree;validateFields()"
         label="Li e Concordo com os Termos de Uso e Privacidade"
       />
       <NormalButton
+        :buttonClass="{ inactive: this.inactive === true }"
         @click.native="register"
         color="var(--yellowHibredu)"
         text="Cadastrar"
@@ -54,17 +69,19 @@
 </template>
 
 <script>
+import globalMethods from "../mixins/globalMethods";
 import NormalButton from "../components/buttons/NormalButton";
 import TextInput from "../components/inputs/TextInput";
 import EmailInput from "../components/inputs/EmailInput";
 import SelectInput from "../components/inputs/SelectInput";
+import SelectCheckBoxInput from "../components/inputs/SelectCheckBoxInput";
 import PasswordInput from "../components/inputs/PasswordInput";
 import CheckBox from "../components/inputs/CheckBox";
 import { mapActions } from "vuex";
 
 export default {
   name: "RegistrationBox",
-
+  mixins: [globalMethods],
   components: {
     NormalButton,
     TextInput,
@@ -72,19 +89,23 @@ export default {
     SelectInput,
     PasswordInput,
     CheckBox,
+    SelectCheckBoxInput,
   },
   data() {
     return {
+      inactive: true,
       email: "",
       name: "",
       phone: "",
       educationalInstitution: [],
-      selectedEducationalInstitution: "",
-      classrooms: "",
+      selectedEducationalInstitution: null,
+      classrooms: null,
       classroomsFormated: [],
+      selectedClassrooms: "",
       password: "",
       password2: "",
       agree: false,
+      selected: [],
     };
   },
   mounted() {
@@ -97,15 +118,25 @@ export default {
       "action_classroomBySchoolId",
     ]),
     register() {
-      this.action_createTeacher({
-        name: this.name,
-        email: this.email,
-        phone: this.phone,
-        password: this.password,
-        school_id: this.selectedEducationalInstitution,
-        classrooms: this.classroomsFormated,
-      }).then(() => {
-      });
+      if (this.password === this.password2) {
+        this.action_createTeacher({
+          name: this.name,
+          email: this.email,
+          phone: this.phone,
+          password: this.password,
+          school_id: this.selectedEducationalInstitution,
+          classrooms: this.selectedClassrooms,
+        })
+          .then(() => {
+            this.$alert("Cadastro realizado com sucesso");
+            this.$router.push("");
+          })
+          .catch(() => {
+            this.$alert("Houve um erro durante o cadastro. Tente novamente");
+          });
+      } else {
+        this.$alert("As senhas não coincidem");
+      }
     },
     getSchools() {
       this.action_school().then((response) => {
@@ -117,14 +148,29 @@ export default {
         schoolId: this.selectedEducationalInstitution,
       }).then((response) => {
         this.classrooms = response;
-        this.formatArray2Register(this.classrooms);
       });
     },
-    formatArray2Register(data) {
+    formatClassrooms2Register(data) {
+      this.selectedClassrooms = [];
       for (let i = 0; i < data.length; i++) {
-        this.classroomsFormated.push({id: data[i].id, name: data[i].name});
+        this.selectedClassrooms.push({ id: data[i] });
       }
-    }
+    },
+    validateFields() {
+      if (
+        this.validateEmail(this.email) &&
+        this.password.length >= 8 &&
+        this.name != null &&
+        this.phone != "" &&
+        this.selectedEducationalInstitution != null &&
+        this.selectedClassrooms != "" &&
+        this.agree != false
+      ) {
+        this.inactive = false;
+      } else {
+        this.inactive = true;
+      }
+    },
   },
 };
 </script>
